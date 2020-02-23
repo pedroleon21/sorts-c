ac39d8d512a3a9a0a3876075429ce9f826a399ed
pedroleon21 <paranayba.clerot@gmail.com>
2020-02-23 17:45:33 -0300

    updade

--- sort.c
+++ sort.c
@@ -8,16 +8,20 @@ void booblesort(long long int *vet,long long int size); // pointer to lli vetcto
 void forprint(long long int *vet,long long int size);// pointer to lli vector && lli size
 void insertionsort(long long int *vet, long long int size); // lli pointer to vector && lli size of vector
 void quicksort(long long int * vet, long long int init, long long int end); // lli poiter to vecto && lli ini  sub lli end
+void mergesort(long long int *vet, long long int init,long long int end); // lli *vet lli init lli end
+void showtime(double *clk); // lli clocks
 
 int main(){
 	//contrucao de um menu
 	long long int size=0,*vet=NULL;
+	double clk = 0;
 	int op;
 	do{
 		if(!size){
 			printf("\n\ndefine a size: ");
 			scanf("%lli", &size);
 		}
+		printf("\nvet size: %lli",size);
 		printf("\n1.........booble");
 		printf("\n2..........merge");
 		printf("\n3..........quick");
@@ -36,23 +40,29 @@ int main(){
 				printf("\nallocating...\n");
 				vet = alloca_vet(size);
 				printf("organizing...\n");
+				showtime(&clk);
 				booblesort(vet,size);
+				showtime(&clk);
 				break;
 			case 2:
-				
+				//mergesort(vet,0,end);
 				break;
 			case 3:
 				if(vet != NULL) {free(vet);}
 				printf("\nallocating");
 				vet = alloca_vet(size);
 				printf("\n\norganizing");
+				showtime(&clk);
 				quicksort(vet,0,size);
+				showtime(&clk);
 				break;
 			case 4:
 				if(vet != NULL) {free(vet);}                                                                                printf("\nallocating...\n");
 				vet = alloca_vet(size);
 				printf("\norganizing....");
+				showtime(&clk);
 				insertionsort(vet,size);
+				showtime(&clk);
 				break;
 			case 5:
 				size = 0;
@@ -62,7 +72,9 @@ int main(){
 			case 6:
 				if(vet != NULL){
 					printf("\n");
+					showtime(&clk);
 					forprint(vet,size);
+					showtime(&clk);
 				}else printf("\nSegmentation fault");
 				break;
 			default:
@@ -72,7 +84,17 @@ int main(){
 	}while(op != 0);
 	return 0;
 }
+/*
 
+void mergesort(long long int *vet,long long int init, long long int end){
+	long long int half = (init+end) /2,a;
+	if(init<end){
+		mergesort(vet,init,half);
+		mergesort(vet,half+1,end);
+	}
+	merge(
+}
+*/
 void quicksort(long long int *vet,long long int init,long long int end){
 	long long int p = vet[(init + end) / 2],in = init, en = end,swap;
 	while(in <= en){
@@ -136,4 +158,10 @@ void insertionsort(long long int *vet, long long int size){
 		}
 	}
 }
-
+void showtime(double * clk){
+	if(!(*clk)) *clk = clock();
+	else{
+		printf("\nT time: %lf",(*clk - (double)clock())/((double)CLOCKS_PER_SEC));
+		*clk = 0;
+	}
+}
